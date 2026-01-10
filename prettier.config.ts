27adc6084b942f45d1b11581d8f602f08323a9e8
newt <hi@newty.dev>
2026-01-10 22:02:17 +0000

    refactor: .prettierrc

--- prettier.config.ts
+++
@@ -1,7 +0,0 @@
-import type { Config } from "prettier";
-
-const plugin = (name: string) => `prettier-plugin-${name}`;
-
-export default {
-  plugins: [plugin("astro"), plugin("svelte"), plugin("tailwindcss")],
-} satisfies Config;
